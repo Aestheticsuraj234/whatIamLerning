eefee5d72c8dd7acafccd647ac1b824f39f17feb
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-03-27 13:08:37 +0530

    tab navigation

--- react-navigation-v8/navigation/Navigation.tsx
+++ react-navigation-v8/navigation/Navigation.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator, createNativeStackScreen } from "@react-navi
 import HomeScreen from "./screens/HomeScreen";
 import { createStaticNavigation } from "@react-navigation/native";
 import SettingScreen from "./screens/SettingScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
 
 
 type RootStackType = typeof RootStack;
@@ -9,6 +11,24 @@ type RootStackType = typeof RootStack;
 declare module "@react-navigation/core"{
     interface RootNavigator extends RootStackType {}
 }
+
+
+const Tabs = createBottomTabNavigator({
+  screens: {
+  Home: {
+      screen: HomeScreen,
+      options: {
+        tabBarIcon: ({ focused, color, size }) => Platform.select({
+          ios: { type: "sfSymbol", name: "house.fill" },
+          android: { type: "materialSymbol", name: "home" },
+          default: { type: "image", source: require("./icons/home.png") },
+        }),
+      },
+    },
+   
+  },
+});
+
 
 const RootStack = createNativeStackNavigator({
     screens:{
